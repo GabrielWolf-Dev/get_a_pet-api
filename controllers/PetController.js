84e7261209382662a1e00aa61e502c577a6bb281
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2023-05-13 19:26:39 -0300

    feat: insert pets

---
+++ controllers/PetController.js
@@ -0,0 +1,75 @@
+const Pet = require("../models/Pet");
+
+// helpers
+const getToken = require("../helpers/get-token");
+const getUserByToken = require("../helpers/get-user-by-token");
+
+module.exports = class PetController {
+  static async create(req, res) {
+    const { name, age, weight, color } = req.body;
+    const avaiable = true;
+    const images = req.files;
+
+    // images upload
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório!" });
+      return;
+    }
+
+    if (!age) {
+      res.status(422).json({ message: "A idade é obrigatória!" });
+      return;
+    }
+
+    if (!weight) {
+      res.status(422).json({ message: "O peso é obrigatório!" });
+      return;
+    }
+
+    if (!color) {
+      res.status(422).json({ message: "A cor é obrigatória!" });
+      return;
+    }
+
+    if (images.length === 0) {
+      res.status(422).json({ message: "A imagem é obrigatória!" });
+      return;
+    }
+
+    // get pet owner
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    // create pet
+    const pet = new Pet({
+      name,
+      age,
+      weight,
+      color,
+      avaiable,
+      images: [],
+      user: {
+        _id: user._id,
+        name: user.name,
+        image: user.image,
+        phone: user.phone,
+      },
+    });
+
+    images.forEach((image) => {
+      pet.images.push(image.filename);
+    });
+
+    try {
+      const newPet = await pet.save();
+      res.status(201).json({
+        message: "Pet cadastrado com sucesso!",
+        newPet,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+};
